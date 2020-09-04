ae4baab01830d4d95914d579d58e7b2e9e6a7aad
Jeon17 <jthomassocial@gmail.com>
2020-09-04 16:26:18 -0400

    new changes made

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
+  
+// Dependency of express
 var express = require("express");
-var bodyParser = require("body-parser");
-var exphbs = require("express-handlebars");
 
-var PORT = process.env.PORT || 8080;
+// Establishing PORT and the app
+var PORT = process.env.PORT || 8000;
 var app = express();
-
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
 // Parse application body
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+//Dependency of handlebars
 var exphbs = require("express-handlebars");
 
+//Setting up handlebars specs
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
@@ -22,6 +24,6 @@ var routes = require("./controllers/burgersController.js");
 
 app.use(routes);
 
-app.listen(PORT, function () {
-    console.log("Server listening on: http://localhost:" + PORT);
+app.listen(PORT, function() {
+  console.log("Listening on port:3000", PORT);
 });
