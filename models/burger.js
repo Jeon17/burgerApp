3f53d5f5ec833325df592dbe139f133d2989c590
Jeon17 <jthomassocial@gmail.com>
2020-09-04 16:20:32 -0400

    refactor on files

--- models/burger.js
+++ models/burger.js
@@ -1,23 +1,29 @@
+  
+//Dependency
 var orm = require("../config/orm.js");
 
+//Burger module used by controller
 var burger = {
-    all: function (cb) {
-        orm.all("burgers", function (res) {
-            cb(res);
-        });
-    },
-    create: function (name, cb) {
-        orm.create("burgers", [
-            "burger_name", "devoured"
-        ], [
-            name, false
-        ], cb);
-    },
-    update: function (id, cb) {
-        var condition = "id=" + id;
-        orm.update("burgers", {
-            devoured: true
-        }, condition, cb);
-    }
+  all: function (cb) {
+    orm.all("burgers", function (res) {
+      cb(res);
+    });
+  },
+  create: function (name, cb) {
+    orm.create("burgers", ["burger_name", "devoured"], [name, false], cb);
+  },
+  update: function (id, cb) {
+    var condition = "id=" + id;
+    orm.update(
+      "burgers",
+      {
+        devoured: true,
+      },
+      condition,
+      cb
+    );
+  },
 };
+
+//Exports burger
 module.exports = burger;
